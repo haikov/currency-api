dae1871764e8088c47bac5ca778f89012102ccaa
Sergey <sdgaykov@gmail.com>
2015-12-24 00:05:01 +0300

    make it better

--- common/models/rate.js
+++ common/models/rate.js
@@ -1,8 +1,8 @@
 var disableAllRestMethods = require('./../../helpers/disableAllRestMethods');
 
 module.exports = function(Rate) {
-  disableAllRestMethods(Rate, ['find', 'findById', 'findOne', '__findById__office', '__get__office',
-  '__findById__currency', '__get__currency']);
+  //disableAllRestMethods(Rate, ['find', 'findById', 'findOne', '__findById__office', '__get__office',
+  //'__findById__currency', '__get__currency']);
 
   Rate.add = function(request, reqObject, cb) {
     var app = Rate.app;
